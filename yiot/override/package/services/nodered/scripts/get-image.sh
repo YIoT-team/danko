157771349940414bd16b3d2afa0b2f73eab333f9
Roman Kutashenko <kutashenko@gmail.com>
2023-08-29 22:37:20 +0300

    Fix init.d for NodeRed

--- yiot/override/package/services/nodered/scripts/get-image.sh
+++ yiot/override/package/services/nodered/scripts/get-image.sh
@@ -21,7 +21,9 @@
 
 readonly SCRIPT_PATH="$(cd $(dirname "$0") >/dev/null 2>&1 && pwd)"
 readonly DST_FILE="${SCRIPT_PATH}/../files/images/nodered.tar"
+readonly IMAGE_INFO_FILE="${SCRIPT_PATH}/../files/images/nodered"
 
+readonly IMAGE="nodered/node-red"
 readonly TAG="3.0.2-14"
 
 if [ "${1}" == "RPi4" ]; then
@@ -33,15 +35,15 @@ fi
 echo "-------------------------------------------------------"
 echo "- Clean NodeRed images"
 echo "-------------------------------------------------------"
-docker image rm nodered/node-red:${TAG} || true
+docker image rm ${IMAGE}:${TAG} || true
 
 echo "-------------------------------------------------------"
 echo "- Pull NodeRed ${TAG}"
 echo "-------------------------------------------------------"
-
-docker pull --platform ${PLATFORM} nodered/node-red:${TAG}
+docker pull --platform ${PLATFORM} ${IMAGE}:${TAG}
+echo "${IMAGE}:${TAG}" > "${IMAGE_INFO_FILE}"
 
 echo "-------------------------------------------------------"
 echo "- Save NodeRed image as a file"
 echo "-------------------------------------------------------"
-docker save -o "${DST_FILE}" nodered/node-red:${TAG}
+docker save -o "${DST_FILE}" ${IMAGE}:${TAG}
