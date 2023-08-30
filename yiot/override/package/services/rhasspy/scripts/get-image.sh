3e89898e730b53298c16b472c169cbf2785e3587
Roman Kutashenko <kutashenko@gmail.com>
2023-08-30 18:55:05 +0300

    Add Rhasspy

---
+++ yiot/override/package/services/rhasspy/scripts/get-image.sh
@@ -0,0 +1,49 @@
+#  ────────────────────────────────────────────────────────────
+#                     ╔╗  ╔╗ ╔══╗      ╔════╗
+#                     ║╚╗╔╝║ ╚╣╠╝      ║╔╗╔╗║
+#                     ╚╗╚╝╔╝  ║║  ╔══╗ ╚╝║║╚╝
+#                      ╚╗╔╝   ║║  ║╔╗║   ║║
+#                       ║║   ╔╣╠╗ ║╚╝║   ║║
+#                       ╚╝   ╚══╝ ╚══╝   ╚╝
+#    ╔╗╔═╗                    ╔╗                     ╔╗
+#    ║║║╔╝                   ╔╝╚╗                    ║║
+#    ║╚╝╝  ╔══╗ ╔══╗ ╔══╗  ╔╗╚╗╔╝  ╔══╗ ╔╗ ╔╗╔╗ ╔══╗ ║║  ╔══╗
+#    ║╔╗║  ║║═╣ ║║═╣ ║╔╗║  ╠╣ ║║   ║ ═╣ ╠╣ ║╚╝║ ║╔╗║ ║║  ║║═╣
+#    ║║║╚╗ ║║═╣ ║║═╣ ║╚╝║  ║║ ║╚╗  ╠═ ║ ║║ ║║║║ ║╚╝║ ║╚╗ ║║═╣
+#    ╚╝╚═╝ ╚══╝ ╚══╝ ║╔═╝  ╚╝ ╚═╝  ╚══╝ ╚╝ ╚╩╩╝ ║╔═╝ ╚═╝ ╚══╝
+#                    ║║                         ║║
+#                    ╚╝                         ╚╝
+#
+#    Lead Maintainer: Roman Kutashenko <kutashenko@gmail.com>
+#  ────────────────────────────────────────────────────────────
+
+#!/bin/bash
+
+readonly SCRIPT_PATH="$(cd $(dirname "$0") >/dev/null 2>&1 && pwd)"
+readonly DST_FILE="${SCRIPT_PATH}/../files/images/rhasspy.tar"
+readonly IMAGE_INFO_FILE="${SCRIPT_PATH}/../files/images/rhasspy"
+
+readonly IMAGE="rhasspy/rhasspy"
+readonly TAG="2.5.11"
+
+if [ "${1}" == "RPi4" ]; then
+    readonly PLATFORM="linux/arm64"
+else
+    readonly PLATFORM="linux/amd64"
+fi
+
+echo "-------------------------------------------------------"
+echo "- Clean Rhasspy images"
+echo "-------------------------------------------------------"
+docker image rm ${IMAGE}:${TAG} || true
+
+echo "-------------------------------------------------------"
+echo "- Pull Rhasspy ${TAG}"
+echo "-------------------------------------------------------"
+docker pull --platform ${PLATFORM} ${IMAGE}:${TAG}
+echo "${IMAGE}:${TAG}" > "${IMAGE_INFO_FILE}"
+
+echo "-------------------------------------------------------"
+echo "- Save Rhasspy image as a file"
+echo "-------------------------------------------------------"
+docker save -o "${DST_FILE}" ${IMAGE}:${TAG}
